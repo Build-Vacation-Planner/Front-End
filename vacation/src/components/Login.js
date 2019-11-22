864985b84e8d33260fd48e936561dea109556755
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-11-21 22:21:41 -0300

    updated styling for sign up and login

--- vacation/src/components/Login.js
+++ vacation/src/components/Login.js
@@ -5,6 +5,8 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { connect } from "react-redux";
+import VacPlanFooter from "./VacPlanFooter";
+
  
 const Login = props => {
 
@@ -28,32 +30,45 @@ const Login = props => {
     }
 
     return (
-        <div>
-        <h2>Login</h2>
-        <StyledForm onSubmit={submitReturningUser}>
-            <StyledH3>Sign in here.</StyledH3>
-            <Label>Enter Username</Label>
-            <StyledInput
-                type='text'
-                name="username"
-                value={returningUser.username}
-                onChange={handleReturningUser}
-                placeholder='username'
-            />
+        <div className="sunset_page">
+            <div className="top">
+                <form className="enter_form" onSubmit={submitReturningUser}>
+                <div className="enter_form_top">
+                    <h3>Sign in here</h3>
+                    <label className="enter_label">Enter Username</label>
+                        <input 
+                    type='text'
+                    name="username"
+                    value={returningUser.username}
+                    onChange={handleReturningUser}
+                    placeholder='username'
+                    className="enter_input"
+                    />
 
-      <Message>Valid username must be entered.</Message>
+                    
 
-            <Label>Enter Password</Label>
-            <StyledInput
-                type="password"
-                name="password"
-                value={returningUser.password}
-                onChange={handleReturningUser}
-                placeholder='password'
-            />
-            <StyledButton >Login</StyledButton>
-        </StyledForm>
-        <StyledParagraph>Don't have an account? Sign up <Link to="/signup">here</Link>.</StyledParagraph>
+                    <label className="enter_label">Enter Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={returningUser.password}
+                        onChange={handleReturningUser}
+                        placeholder='password'
+                        className="enter_input"
+                    />
+                    <br />
+                    <Message>Valid username must be entered.</Message>
+                </div>
+            
+                <button className="enter_button">Login</button>
+                <p className="already">Don't have an account? Sign up <Link to="/signup">here</Link>.</p>
+                </form>
+        
+            </div>
+       
+        <footer className="footer">
+            <VacPlanFooter />
+        </footer>
         </div>
     );
 }
@@ -68,60 +83,60 @@ function validateUsername(username) {
 
 export default Login;
 
-const Label = Styled.label`
-    text-align: left;
-    margin-left: 30px;
-`;
+// const Label = Styled.label`
+//     text-align: left;
+//     margin-left: 30px;
+// `;
 
-const StyledInput = Styled.input`
-    width: 200px;
-    padding: 5px 20px;
-    margin: 0 auto;
-    font-size: 14px;
-    color: black;
-    background: #fff;
-    border: 1px solid #fff;
-    border-radius: 5px;
-`;
+// const StyledInput = Styled.input`
+//     width: 200px;
+//     padding: 5px 20px;
+//     margin: 0 auto;
+//     font-size: 14px;
+//     color: black;
+//     background: #fff;
+//     border: 1px solid #fff;
+//     border-radius: 5px;
+// `;
 
-const StyledH3 = Styled.h3`
-    text-align: left;
-`;
+// const StyledH3 = Styled.h3`
+//     text-align: left;
+// `;
 
-const StyledButton = Styled.button`
-    background: #0500FF;
-    border-color: transparent;
-    color: #fff;
-    cursor: pointer;
-    width: 80%
-    margin: 0 auto;
-    margin-top:25px;
-    font-weight:bold;
-    font-size:16px;
-    height:50px;
-    border-radius:5px
-    &:hover{
-    background:#111125
-    }
-`;
+// const StyledButton = Styled.button`
+//     background: #0500FF;
+//     border-color: transparent;
+//     color: #fff;
+//     cursor: pointer;
+//     width: 80%
+//     margin: 0 auto;
+//     margin-top:25px;
+//     font-weight:bold;
+//     font-size:16px;
+//     height:50px;
+//     border-radius:5px
+//     &:hover{
+//     background:#111125
+//     }
+// `;
 
-const StyledParagraph = Styled.div`
-    text-align: center;
-    margin-top: 20px;
-    `;
+// const StyledParagraph = Styled.div`
+//     text-align: center;
+//     margin-top: 20px;
+//     `;
 
-const StyledForm = Styled.form`
-    padding: 0 30px 25px 30px;
-    width: 300px;
-    margin: 0 auto;
-    text-align: left;
-    background: #f3f3f3;
-    border: 1px solid #fff;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`;
+// const StyledForm = Styled.form`
+//     padding: 0 30px 25px 30px;
+//     width: 300px;
+//     margin: 0 auto;
+//     text-align: left;
+//     background: #f3f3f3;
+//     border: 1px solid #fff;
+//     border-radius: 5px;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+// `;
 
 const Message = Styled.label`
   margin-bottom: 2px;
---
+++ vacation/src/components/VacPlanFooter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VacPlanFooter = () => {
+    return (
+        <h1 className="vac_plan_foot_text">Vacation Planner</h1>
+    )
+}
+
+export default VacPlanFooter;
